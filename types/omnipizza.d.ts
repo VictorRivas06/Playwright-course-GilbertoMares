51307ff414c65849190ef51f611755fd70e1c973
Victor Rivas <vic.rivas.1999@gmail.com>
2026-07-18 11:51:53 -0700

    Data driven

--- types/omnipizza.d.ts
+++ types/omnipizza.d.ts
@@ -1,3 +1,26 @@
 
 export type CountryCode = "MX" | "US"  | "CH" | "JP"
 export type Currency = "MXN" | "USD" | "CHF" | "JYP"
+<<<<<<< Updated upstream
+=======
+export type Role = "customer"
+
+export interface User{
+    username: string;
+    password: string;
+    role?: Role;
+    description?: string;
+}
+
+export interface Market{
+    code: CountryCode,
+    currency: Currency,
+    fullName: string;
+    country: string;
+    phone: string;
+    address: string;
+    colonia?: string;
+    zipCode: string;
+    taxRate?: number;
+}
+>>>>>>> Stashed changes
